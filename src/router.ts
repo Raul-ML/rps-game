c992c71738156a9bfc612e5b56f5b1e2a205a7fe
Raul-ML <raulmendezlopez0@gmail.com>
2025-01-07 22:19:59 +0100

    backup

--- src/router.ts
+++ src/router.ts
@@ -27,17 +27,27 @@ const routes = [
   ];
 
 
+  const BASE_PATH = "/rps-game";
 
-
-  export function initRouter(container: Element) {
-    function goTo(path: string) {
-        history.pushState({}, "", path);
-        handleRoute(path);
-    }
+  function isGithubPages() {
+    return location.host.includes("github.io");
+  }
   
+  export function initRouter(container: Element) {
+    function goTo(path) {
+      // el goTo va a recibir la ruta de siempre: /jugar
+      // por eso, en el caso de GitHub Pages
+      // debemos anteponerle el BASE_PATH para que funcione
+      // en ese contexto
+      const completePath = isGithubPages() ? BASE_PATH + path : path;
+      history.pushState({}, "", completePath);
+      handleRoute(completePath);
+    }
     function handleRoute(route) {
+      const newRoute = isGithubPages() ? route.replace(BASE_PATH, "") : route;
+
       for (const r of routes) {        
-        if (r.path.test(route)) {
+        if (r.path.test(newRoute)) {
           const el = r.component({ goTo: goTo });
           container.firstChild?.remove();
           container.appendChild(el);    
@@ -45,7 +55,7 @@ const routes = [
       }
     }
   
-    if (location.pathname == "/") {
+    if (location.pathname == "/"||location.pathname == BASE_PATH+"/") {
       goTo("/welcome");
     } else {
       handleRoute(location.pathname);
